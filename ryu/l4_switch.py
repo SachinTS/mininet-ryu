60df41dffa2a2f3f1238372e1579929900d45cd9
sachin <sachintshebbar@gmail.com>
2020-08-28 14:01:09 +0100

    Update code with only needed part

--- ryu/l4_switch.py
+++ ryu/l4_switch.py
@@ -42,19 +42,6 @@ from ryu.app.wsgi import ControllerBase, WSGIApplication, route
 
 import json
 
-
-# Name for registry of API
-
-learning_switch_instance = 'learning-switch-api'
-
-
-# Base URL for the API
-
-# e.g. http://localhost:8080/learningswitch/api/v1/mappings/0000000000000001 for switch (datapath) 1
-
-url = '/learningswitch/api/v1/'
-
-
 class LearningSwitch(app_manager.RyuApp):
 
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -73,21 +60,6 @@ class LearningSwitch(app_manager.RyuApp):
 
         self.communication_per_host = {}
 
-        # # Register Rest API
-        #
-        # wsgi = kwargs['wsgi']
-        #
-        # wsgi.register(LearningSwitchRest, {learning_switch_instance: self})
-        #
-        # self.monitor_thread = hub.spawn(self.clean_stats)
-
-    def clean_stats(self):
-
-        while True:
-
-            self.communication_per_host = {}
-
-            hub.sleep(1)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -108,31 +80,6 @@ class LearningSwitch(app_manager.RyuApp):
 
         self.add_flow(datapath, 0, match, actions)
 
-        ##
-
-        # Apply the rules to the firewall
-
-        # In order to install defulat firewall you should specify the firewall id in the net
-
-        # rules were not applied in the loop for further manipulation power which was useful even for me in debugging process
-
-        ##
-
-        if datapath.id == 2:
-
-            actionss = []
-
-            self.add_flow(datapath, 2, self.match_from_json(
-                "/home/vagrant/base/base/rules.json", datapath, 1), actionss)
-
-            self.add_flow(datapath, 2, self.match_from_json(
-                "/home/vagrant/base/base/rules.json", datapath, 2), actionss)
-
-            self.add_flow(datapath, 2, self.match_from_json(
-                "/home/vagrant/base/base/rules.json", datapath, 3), actionss)
-
-            self.add_flow(datapath, 2, self.match_from_json(
-                "/home/vagrant/base/base/rules.json", datapath, 4), actionss)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -346,148 +293,3 @@ class LearningSwitch(app_manager.RyuApp):
         #self.logger.info("FLOW MOD: Written")
 
         datapath.send_msg(mod)
-
-    def add_flow_with_block(self, datapath, priority, match, actions, timeout):
-
-        ofproto = datapath.ofproto
-
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(
-            ofproto.OFPIT_APPLY_ACTIONS, actions)]
-
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-
-                                match=match, instructions=inst, hard_timeout=timeout)
-
-        #self.logger.info("FLOW MOD: Written")
-
-        datapath.send_msg(mod)
-
-    def print_keyword_args(self, kwargs):
-
-        # kwargs is a dict of the keyword args passed to the function
-
-        for key, value in kwargs.iteritems():
-
-            print(key + " = " + value)
-
-            return key + "=" + value
-
-    def match_from_json(self, file_name, datapath, rule_number):
-        ''' Your JSON file must have all required fields '''
-
-        # For the file_name, try using the whole path (e.g. /home/vagrant/Firewall-CW/rules.json)
-
-        try:
-
-            with open(file_name, "r") as rules_file:
-
-                rules_dict = json.load(rules_file)
-
-            if rules_dict['datapath'] == None:
-
-                self.logger.info("Invalid Rules File!")
-
-                return
-
-            #key,value = rules_dict["datapath"]
-
-            # print(str(**rules_dict["datapath"]["2"]))
-
-            # print(**rules_dict)
-
-            ###
-
-            # self.print_keyword_args(**rules_dict["datapath"])
-
-            rule = 0
-
-            rules = {}
-
-            # Loop that returns patch for specified rule
-
-            for lists in rules_dict["datapath"]["2"]:
-
-                rule += 1
-
-                rules = {}
-
-                for element, value in lists.iteritems():
-
-                    if(element == "description"):
-
-                        # print(value)
-
-                        pass
-
-                    elif element == "action":
-
-                        pass
-
-                    else:
-
-                        rules[element] = value
-
-                if(rule == rule_number):
-
-                    break
-
-            # print(rules)
-
-            # Parse your rules json here, make sure you only add the valid fields
-
-            # You should also add validation here!
-
-            # Look out for missing fields (e.g. If you use L3 or L4 match rules, be sure eth type is added)
-
-            # TIP: Look at how a python dictionary differs from the JSON format
-
-            ###
-
-            # if key == datapath.id:
-
-                # pass
-
-            print(rules)
-
-            return datapath.ofproto_parser.OFPMatch(**rules)
-
-        except:
-
-            self.logger.info("Rules File \"" + file_name +
-                             "\" Failed to Parse")
-
-            # return datapath.ofproto_parser.parser.OFPMatch()
-
-
-class LearningSwitchRest(ControllerBase):
-
-    def __init__(self, req, link, data, **config):
-
-        super(LearningSwitchRest, self).__init__(req, link, data, **config)
-
-        # Get the switch instance
-
-        self.switch = data[learning_switch_instance]
-
-    @route('learningswitch', url + "mappings/{dpid}", methods=['GET'], requirements={'dpid': dpid_lib.DPID_PATTERN})
-    def get_switch_mappings(self, req, **kwargs):
-
-        dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
-
-        # Invalid DPID
-
-        if dpid not in self.switch.mac_to_port:
-
-            return Response(status=404)
-
-        # Write mac to port table into json for a given dpid
-
-        mac_table = self.switch.mac_to_port.get(dpid, {})
-
-        body = json.dumps(mac_table)
-
-        # Return over the network as a response to the GET
-
-        return Response(content_type='application/json', body=body)
